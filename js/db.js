eb5cb554b9f12508fd7f0ce4456f30da7ab8a63e
xovest <79008917+xovest@users.noreply.github.com>
2021-12-20 12:50:43 +0200

    data added

--- js/db.js
+++ js/db.js
@@ -21,3 +21,20 @@ db.collection('recipes').onSnapshot(snapshot => {
     }
   });
 });
+
+// add new recipe
+const form = document.querySelector('form');
+form.addEventListener('submit', evt => {
+  evt.preventDefault();
+  
+  const recipe = {
+    name: form.title.value,
+    ingredients: form.ingredients.value
+  };
+
+  db.collection('recipes').add(recipe)
+    .catch(err => console.log(err));
+
+  form.title.value = '';
+  form.ingredients.value = '';
+});
